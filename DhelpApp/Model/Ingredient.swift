9eb06596df680b97265b3eac0984c0d062ee7fb2
rezaf <rezaf>
2021-04-01 23:51:25 +0700

    Init

---
+++ DhelpApp/Model/Ingredient.swift
@@ -0,0 +1,15 @@
+//
+//  Ingredient.swift
+//  DhelpApp
+//
+//  Created by Reza Harris on 01/04/21.
+//
+
+import Foundation
+
+struct Ingredient: Codable {
+    let name: String
+    let sugar: Double
+    let carbs: Double
+    let calorie: Double
+}
---
+++ DhelpApp/View/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  DhelpApp
+//
+//  Created by Reza Harris on 01/04/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
