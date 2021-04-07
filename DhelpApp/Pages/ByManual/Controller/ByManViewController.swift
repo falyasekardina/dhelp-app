b7aad91e6461d539b1a954fa36692bfe934510ed
Falya Aqiela Sekardina <falyasekardina@gmail.com>
2021-04-07 12:19:23 +0700

    add by manual swift code

--- DhelpApp/Pages/ByManual/Controller/ByManViewController.swift
+++ DhelpApp/Pages/ByManual/Controller/ByManViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ByManViewController: UIViewController {
+    
+    @IBOutlet weak var addFoodTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addFoodTable.delegate = self
+        addFoodTable.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -26,4 +30,70 @@ class ByManViewController: UIViewController {
     }
     */
 
+}
+
+extension ByManViewController: UITableViewDelegate, UITableViewDataSource {
+    struct ListOfFields {
+        static var fieldName = [
+            "Food Name",
+            "Calories",
+            "Carbs",
+            "Sugar",
+            "Serving Size"
+        ]
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var sectionIndex : Int = 0
+        if section == 0
+        {
+            sectionIndex = 1
+        }
+        else if section == 1
+        {
+            sectionIndex = 5
+        }
+        else if section == 2
+        {
+            sectionIndex = 2
+        }
+        return sectionIndex
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cellTime = tableView.dequeueReusableCell(withIdentifier: "time", for: indexPath)
+            cellTime.textLabel?.text = "Time"
+            
+            return cellTime
+        }
+        
+        else if indexPath.section == 1 {
+            let cellDetail = tableView.dequeueReusableCell(withIdentifier: "detail", for: indexPath)
+            cellDetail.textLabel?.text = ListOfFields.fieldName[indexPath.row]
+            
+            return cellDetail
+        }
+        
+        return UITableViewCell()
+        
+    }
+    
+    private func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            var headerTitle : String?
+            if section == 0
+            {
+                headerTitle = "Time"
+            }
+            if section == 1
+            {
+                headerTitle = "Detail"
+            }
+            return headerTitle
+        }
+
+    //Below method wil return number of sections
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 3
+    }
 }
