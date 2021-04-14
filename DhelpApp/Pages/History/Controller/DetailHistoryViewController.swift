5533c0fe8ffce039a990a1c4ee34b7db32277ea7
Reza F <rezafh19@gmail.com>
2021-04-14 10:56:06 +0700

    History Done!

--- DhelpApp/Pages/History/Controller/DetailHistoryViewController.swift
+++ DhelpApp/Pages/History/Controller/DetailHistoryViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 struct Section{
     var header: String
@@ -24,13 +25,18 @@ struct CustomCell{
 struct DetailHistory{
     var item: String
     var gram: [String]?
+    var full: Intake
 }
 
 class DetailHistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    var dataCalories = "20 kcal"
-    var dataGula = "20 gr"
-    var dataCarb = "200 gr"
+    var dataCalories = ""
+    var dataGula = ""
+    var dataCarb = ""
+    
+    var dataHistory: DataHistory?
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 
     @IBOutlet weak var dateDetailHistory: UILabel!
@@ -48,44 +54,45 @@ class DetailHistoryViewController: UIViewController, UITableViewDelegate, UITabl
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configure()
-        
+//        configure()
+        fecthData()
+        dataCalories = "\(dataHistory!.totalCal) kcal"
+        dataGula = "\(dataHistory!.totalSugar) gr"
+        dataCarb = "\(dataHistory!.totalCarbs) gr"
+        dateDetailHistory.text = "\(dateFormater(dateData: dataHistory!.dates))"
+
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
-       
         
         tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier)
-        
-        
     }
     
-    func configure(){
-        models.append(Section(header: "", list: [
-            .switchCell(model: CustomCell(namaGram: ""))
-        ]))
-        
-        models.append(Section(header: "Breakfast", list: [
-            .staticCell(model: DetailHistory(item: "Nasi Putih", gram: ["16 gr"])),
-            .staticCell(model: DetailHistory(item: "Wortel", gram: ["4 gr"])),
-        ]))
-        
-        models.append(Section(header: "Lunch", list: [
-            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
-            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
-        ]))
-        
-        models.append(Section(header: "Dinner", list: [
-            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
-            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
-        ]))
-        
-        models.append(Section(header: "Snack", list: [
-            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
-            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
-        ]))
-    }
+//    func configure(){
+//        models.append(Section(header: "", list: [
+//            .switchCell(model: CustomCell(namaGram: ""))
+//        ]))
+//
+//        models.append(Section(header: "Breakfast", list: [
+//            .staticCell(model: DetailHistory(item: "Nasi Putih", gram: ["16 gr"])),
+//            .staticCell(model: DetailHistory(item: "Wortel", gram: ["4 gr"])),
+//        ]))
+//
+//        models.append(Section(header: "Lunch", list: [
+//            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
+//            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
+//        ]))
+//
+//        models.append(Section(header: "Dinner", list: [
+//            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
+//            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
+//        ]))
+//
+//        models.append(Section(header: "Snack", list: [
+//            .staticCell(model: DetailHistory(item: "Ayam Goreng", gram: ["30 gr"])),
+//            .staticCell(model: DetailHistory(item: "bayam", gram: ["20 gr"])),
+//        ]))
+//    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
@@ -134,8 +141,77 @@ class DetailHistoryViewController: UIViewController, UITableViewDelegate, UITabl
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        switch models[indexPath.section].list[indexPath.row] {
+        case .staticCell(let model):
+            performSegue(withIdentifier: "gotoDetailItem", sender: model.full)
+        case .switchCell(let model):
+            print(model.namaGram)
+        }
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? DetailItemHistoryViewController {
+            guard let ingredient = sender as? Intake else {
+                return
+            }
+            destinationVC.getIntake = ingredient
+        }
+    }
+    
+    func fecthData() {
+        do {
+            let request = Intake.fetchRequest() as NSFetchRequest<Intake>
+            let datas = try context.fetch(request)
+            
+            var dataBreakfast = [CellType]()
+            var dataLunch = [CellType]()
+            var dataDinner = [CellType]()
+            var dataSnack = [CellType]()
+            
+            for dt in datas {
+                if getDayFormater(dateData: dt.createdat!) == getDayFormater(dateData: dataHistory!.dates) {
+                    var newData = CellType.staticCell(model: DetailHistory(item: dt.name!, gram: ["\(dt.sugar) gr"], full: dt))
+                    if dt.mealtime == "Breakfast" {
+                        dataBreakfast.append(newData)
+                    } else if dt.mealtime == "Lunch" {
+                        dataLunch.append(newData)
+                    } else if dt.mealtime == "Dinner" {
+                        dataDinner.append(newData)
+                    } else if dt.mealtime == "Snack" {
+                        dataSnack.append(newData)
+                    }
+                }
+            }
+            models.append(Section(header: "", list: [.switchCell(model: CustomCell(namaGram: ""))]))
+            models.append(Section(header: "Breakfast", list: dataBreakfast))
+            models.append(Section(header: "Lunch", list: dataLunch))
+            models.append(Section(header: "Dinner", list: dataDinner))
+            models.append(Section(header: "Snack", list: dataSnack))
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    func dateFormater(dateData: Date) -> String {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "MMMM, yyyy"
+        dateFormater.timeStyle = .none
+        dateFormater.dateStyle = .medium
+        dateFormater.timeZone = TimeZone.current
+        let newFormat = dateFormater.string(from: dateData)
+        return newFormat
+    }
+    
+    func getDayFormater(dateData: Date) -> String {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy-MM-dd"
+        let newFormat = dateFormater.string(from: dateData)
+        return newFormat
+    }
    
 }
 
